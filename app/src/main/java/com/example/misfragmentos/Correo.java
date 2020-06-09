c42933f4ab88d29d272735ba1593b043a0cb832c
nilder <nituso100@hotmail.com>
2020-06-08 20:36:13 -0500

    Recivler View con Asunto de correos enviados

---
+++ app/src/main/java/com/example/misfragmentos/Correo.java
@@ -0,0 +1,39 @@
+package com.example.misfragmentos;
+
+public class Correo {
+    private String de;
+    private String asunto;
+    private String texto;
+
+
+
+    public String getDe() {
+        return de;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Correo(String de, String asunto, String texto) {
+        this.de = de;
+        this.asunto = asunto;
+        this.texto = texto;
+    }
+}
